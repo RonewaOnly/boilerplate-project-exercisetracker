9d98f075976efddfcc589481f00b36703c523cc1
Ronewa Maselesele <idontgiveaf31@gmail.com>
2024-12-25 20:07:50 +0000

    `Refactor MongoDB connection logic and update MONGO_URI in sample.env`

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const mongoURI = process.env.MONGO_URI;
+
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 // User Schema
 const userSchema = new mongoose.Schema({
